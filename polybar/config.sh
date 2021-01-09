653913ae6ca8bbd097043c024633c323f23d2c17
SolalVall <solal.vallee@gmail.com>
2021-01-09 17:09:18 -0500

    [UPDATE] modify config of polybar/i3, add pre-commit, add urxvt package

--- polybar/config.sh
+++ polybar/config.sh
@@ -1,37 +1,50 @@
-POLYBAR_THEME=polybar-6
+POLYBAR_THEME=hack
 POLYBAR_CONFIG_LOCATION=$HOME/.config/polybar
 POLYBAR_TOOL_LOCATION=$HOME/.lazyTools.d/polybar
 POLYBAR_CONFIG_FILE="polybar-config"
 
 ###
+# Install dependencies
+###
+sudo apt install pywal calc networkmanager_dmenurofi
+
+###
 # Configure polybar in i3 with a theme (https://github.com/adi1090x/polybar-themes)
 ##
 ## Clone
-git clone https://github.com/adi1090x/polybar-themes
-cd polybar-themes/$POLYBAR_THEME
+git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
+
+## Execute base script
+cd polybar-themes
+chmod +x setup.sh
+./setup.sh
+
+## Launch theme
+bash ~/.config/polybar/launch.sh --$POLYBAR_THEME
+#cd polybar-themes/$POLYBAR_THEME
 
 ## Encsure fonts folder
-mkdir -p $HOME/.local/share/fonts
+#mkdir -p $HOME/.local/share/fonts
 
 ## font bitmap reconfig
-rm /etc/fonts/conf.d/70-no-bitmaps.conf
+#rm /etc/fonts/conf.d/70-no-bitmaps.conf
 
 ## Add fonts
-cp -r fonts/* $HOME/.local/share/fonts
+#cp -r fonts/* $HOME/.local/share/fonts
 
 ## Reload fonts cache
-fc-cache -v
+#fc-cache -v
 
 ## Copy config
-cp -r * $POLYBAR_CONFIG_LOCATION
+#cp -r * $POLYBAR_CONFIG_LOCATION
 
-## Copy custom config
-cp $POLYBAR_TOOL_LOCATION/$POLYBAR_CONFIG_FILE $POLYBAR_CONFIG_LOCATION/config.ini
+## Create symlink to default polybar config file location named config.ini (allow to modify the config file from .lazytools.d folder)
+#ln -s $POLYBAR_TOOL_LOCATION/$POLYBAR_CONFIG_FILE $POLYBAR_CONFIG_LOCATION/config.ini
 ## Launch script
-$POLYBAR_CONFIG_LOCATION/launch.sh
+#$POLYBAR_CONFIG_LOCATION/launch.sh
 
 ###
 # Clean
 ###
-cd ../../
-rm -rf polybar-themes
+#cd ../../
+#rm -rf polybar-themes
---
+++ rxvt-unicode/config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+URXVT_CONFIG_LOCATION=$HOME/.urvxt
+URXVT_EXTENSION_LOCATION=$URXVT_CONFIG_LOCATION/ext
+URXVT_TOOL_LOCATION=$HOME/.lazyTools.d/rxvt-unicode
+URXVT_CONFIG_FILE="urxvt-config"
+
+###
+# Configure urxvt
+###
+## Ensure urxvt default config folder + extension fodler
+mkdir -p $URXVT_EXTENSION_LOCATION
+
+## Download urxvt clipboad-manager and Perl extension
+sudo apt install xsel
+git clone https://github.com/muennich/urxvt-perls
+cp urxvt-perls/deprecated/* $URXVT_EXTENSION_LOCATION
+rm -rf urxvt-perls
+
+## Symlink urxvt config file to default location (.Xresources is the default name)
+## Create symlink to default urxvt config file location named .Xresources (allow to modify the config file from .lazytools.d folder)
+ln -s $URXVT_TOOL_LOCATION/$URXVT_CONFIG_FILE $HOME/.Xresources
